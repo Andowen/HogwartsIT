7a22ff760b0086e52000f2e1ffb5b3bbb662afba
Andowen <46093390+Andowen@users.noreply.github.com>
2018-12-27 19:20:25 +0100

    Förbättrad mainmetod med kommentarer

--- src/hogwartsit/Main.java
+++ src/hogwartsit/Main.java
@@ -5,28 +5,35 @@
  */
 
 package hogwartsit;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
 import javax.swing.JOptionPane;
 import oru.inf.InfDB;
 import oru.inf.InfException;
 /**
  *
- * @author andow
+ * @author Anna Svensson och Ellinor Danielsson
  */
 public class Main {
     
     private static InfDB hogwartsDatabaskoppling;
- 
+    
+    // mainmetoden startar applikationen 
     public static void main(String[] args){
         System.out.println("Applikationen startar.");
         
         try {
-            Path dbPath = FileSystems.getDefault().getPath("HOGDB.FDB").toAbsolutePath();
-            System.out.println(dbPath);
-            hogwartsDatabaskoppling = new InfDB(dbPath.toString());
+            // Söker efter filmappen som applikationen körs från och letar specifikt upp databasfilen
+            String databasSokvag = FileSystems.getDefault().getPath("HOGDB.FDB").toAbsolutePath().toString();
+            System.out.println(databasSokvag);
+            
+            // Instansierar ett nytt objekt av typen InfDB, med databasens sökväg som inparameter
+            hogwartsDatabaskoppling = new InfDB(databasSokvag);
+            
+            // Instansierar och öppnar ett nytt Huvudfönster med ett objekt av typen InfDB som inparameter
             new HuvudFonster(hogwartsDatabaskoppling).setVisible(true);
                     
          } catch(InfException ettUndantag){
+            //Visar ett felmeddelande om ett undantag upptäcks vid körning av metoden.
             JOptionPane.showMessageDialog(null, "Något gick visst fel!");
             System.out.println("Internt felmeddelande: " + ettUndantag.getMessage());
         }
